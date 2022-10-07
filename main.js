e36e38f1d9271e443e4e2be252587ff943e10d34
Callasaurus <c.sullivandrage@outlook.com>
2022-10-07 10:09:03 +0100

    basic page created

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
+let h1 = document.querySelector("#dad-joke");
+
 async function getJoke(){
     let response = await fetch("https://icanhazdadjoke.com/", {
         headers: { accept: "application/json" },
       });
     let data = await response.json();
-    console.log(data)
+    h1.textContent = data.joke 
 }
 getJoke();
 
-//PLAN - Create a website similar to the dad jokes website and have it come up with random jokes
+let qouteButton = document.querySelector("#new-quote-button")
+
+qouteButton.addEventListener("click", getJoke)
+qouteButton.addEventListener("click", logJoke)
+ 
+ function logJoke() {
+    let jokeHistory = document.querySelector("#joke-history");
+    let logJoke = document.createElement("li"); 
+    logJoke.textContent = h1.textContent;
+    jokeHistory.appendChild(logJoke);
+}
+
+// PLAN - Create a website similar to the dad jokes website and have it come up with random jokes
 // When we click a button a new joke pops up 
-//First Step - Look back at the kanye workshop
+// First Step - Look back at the kanye workshop
 // Build the physical website (not yet clickable)
 // Title, Image of a Dad, H1 element to print the joke,button to get joke
-//ul element to list the previous jokes under.
-//
+// ul element to list the previous jokes under
+
+// Find image of machine / van & manipulate it with CSS animations
+// When we click the button it will shake 
+// Shoot out the dad joke 
